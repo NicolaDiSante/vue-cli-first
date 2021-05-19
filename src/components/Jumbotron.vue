3bb29b2ca3f4a80402ecd06867433388e1299533
NicolaDiSante <nickymacha90@gmail.com>
2021-05-19 18:05:04 +0200

    Completato

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="jumbotron-box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Jumbotron'
+}
+</script>
+
+<style>
+
+.jumbotron-box{
+  height: 200px;
+  width: 500px;
+  background-color: aqua;
+  margin: auto;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <header>
+      <h1>La mia prima APP con VUE!</h1>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title'
+}
+</script>
+
+<style scoped>
+
+h1{
+  color: blue;
+  text-align: center;
+}
+
+</style>
